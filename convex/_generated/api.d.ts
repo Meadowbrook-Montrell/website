ebb35358c458217954e1d25f3792888b1b98f9a9
Meadowbrook-Montrell <makeitmakesensepodcast@atomicmail.io>
2026-05-03 18:59:05 +0000

    chore: add convex/_generated/api.d.ts for Netlify build

---
+++ convex/_generated/api.d.ts
@@ -0,0 +1,75 @@
+/* eslint-disable */
+/**
+ * Generated `api` utility.
+ *
+ * THIS CODE IS AUTOMATICALLY GENERATED.
+ *
+ * To regenerate, run `npx convex dev`.
+ * @module
+ */
+
+import type * as ViktorSpacesEmail from "../ViktorSpacesEmail.js";
+import type * as admin from "../admin.js";
+import type * as auth from "../auth.js";
+import type * as businessOps from "../businessOps.js";
+import type * as commandCenter from "../commandCenter.js";
+import type * as constants from "../constants.js";
+import type * as consumer from "../consumer.js";
+import type * as contentLib from "../contentLib.js";
+import type * as http from "../http.js";
+import type * as operations from "../operations.js";
+import type * as seedTestUser from "../seedTestUser.js";
+import type * as testAuth from "../testAuth.js";
+import type * as users from "../users.js";
+import type * as viktorTools from "../viktorTools.js";
+
+import type {
+  ApiFromModules,
+  FilterApi,
+  FunctionReference,
+} from "convex/server";
+
+declare const fullApi: ApiFromModules<{
+  ViktorSpacesEmail: typeof ViktorSpacesEmail;
+  admin: typeof admin;
+  auth: typeof auth;
+  businessOps: typeof businessOps;
+  commandCenter: typeof commandCenter;
+  constants: typeof constants;
+  consumer: typeof consumer;
+  contentLib: typeof contentLib;
+  http: typeof http;
+  operations: typeof operations;
+  seedTestUser: typeof seedTestUser;
+  testAuth: typeof testAuth;
+  users: typeof users;
+  viktorTools: typeof viktorTools;
+}>;
+
+/**
+ * A utility for referencing Convex functions in your app's public API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = api.myModule.myFunction;
+ * ```
+ */
+export declare const api: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "public">
+>;
+
+/**
+ * A utility for referencing Convex functions in your app's internal API.
+ *
+ * Usage:
+ * ```js
+ * const myFunctionReference = internal.myModule.myFunction;
+ * ```
+ */
+export declare const internal: FilterApi<
+  typeof fullApi,
+  FunctionReference<any, "internal">
+>;
+
+export declare const components: {};
